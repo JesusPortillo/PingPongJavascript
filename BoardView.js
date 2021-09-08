53f0c78c3007480fd792c15ea80858bd0c3c9a03
JesusPortillo <jesusdavidpv12@gmail.com>
2021-09-08 11:47:34 -0500

    Clase boardView creada y exportada

---
+++ BoardView.js
@@ -0,0 +1,9 @@
+class BoardView{
+    constructor(canvas, board){
+        this.canvas = canvas;
+        this.canvas.width = board.width;
+        this.canvas.height = board.height;
+        this.board = board;
+        this.ctx = canvas.getContext("2d");
+    }
+}export {BoardView};
