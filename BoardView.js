c84466629c5d7e31e31991bef359da3cf41854bf
JesusPortillo <jesusdavidpv12@gmail.com>
2021-09-08 13:59:35 -0500

    Funcion para dibujar las barras desarrolladas

--- BoardView.js
+++ BoardView.js
@@ -7,5 +7,22 @@ class BoardView{
         this.ctx = canvas.getContext("2d");
     }
 
-    
+    drawBoardView(){
+        for (let i = this.board.elements.length - 1; i >= 0 ; i--) {
+            const ele = this.board.elements[i];
+
+            this.draw(this.ctx, ele);
+            
+        }
+    }
+
+    draw(ctx, element){
+        if (element !== null && element.hasOwnProperty("kind")) {
+            switch (element.kind) {
+                case "rectangle":
+                    ctx.fillRect(element.x, element.y, element.width, element.height);
+                    break;
+            }
+        }
+    }
 }export {BoardView};
